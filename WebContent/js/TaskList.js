31715b512bf4ce6234dcd2343a51c721fe9c3750
Iselin Thorsen Nilsen <iselin.thorsen.nilsen@gmail.com>
2021-09-07 14:55:23 +0200

    Setup of TaskList

---
+++ WebContent/js/TaskList.js
@@ -0,0 +1,70 @@
+/**
+ * TaskList from exercise on components
+ */
+
+class TaskList {
+	constructor(task) {
+		// Ikke nødvendigvis at vi trenger noe her
+		this.task = task;
+	}
+	
+}
+
+// Metoder som skal implementeres
+
+// Vet at det er stavet feil her, men siden det er dette navnet Bjarte har brukt så gjør jeg det også
+function enableaddtask() {
+		
+	return null;	
+}
+
+function addtaskCallback() {
+	
+	return null;
+}
+
+function changestatusCallback() {
+	
+	return null;
+}
+
+function deletetaskCallback() {
+	
+	return null;
+}
+
+function noTask() {
+	
+	return null;
+}
+
+function showTask() {
+	
+	return null;
+}
+
+function updateTask() {
+	
+	return null;
+}
+
+function removeTask() {
+	
+	return null;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
