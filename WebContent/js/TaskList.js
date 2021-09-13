16b128d509b9ac22b5d961538e8d41a66cce9055
Cathrine Monsen <monsen.cathrine@gmail.com>
2021-09-13 15:57:25 +0200

    add module

--- WebContent/js/TaskList.js
+++ WebContent/js/TaskList.js
@@ -1,14 +1,18 @@
-/**
- * TaskList from exercise on components
- */
-
-class TaskList {
-	constructor(task) {
+export default class extends HTMLElement {
+	constructor() {
 		// Ikke nødvendigvis at vi trenger noe her
-		this.task = task;
+		this.content = this.content.bind(this);
+		const shadow = this.attachShadow({ mode: 'closed' });
+		shadow.appendChild(this.content);
+
+		const tasklist = document.querySelector("task-list");
+		tasklist._enableaddtask();
 	}
-	
+
+
+
 }
+
 
 const taskList;
 
@@ -17,41 +21,41 @@ const taskList;
 // Vet at det er stavet feil her, men siden det er dette navnet Bjarte har brukt så gjør jeg det også
 // Denne skal oppdatere view
 function enableaddtask() {
-	
+
 	// After button is pushed the task is enabled?
 	// Kan være noe vanskelig her da det er vanskelig å tolke hva som ligger i "enabled"
 	// Denne metoden skal enable "New task" knappen
-	
-	return null;	
+
+	return null;
 }
 
 function addtaskCallback() {
-	
+
 	return null;
 }
 
 function changestatusCallback() {
-	
+
 	return null;
 }
 
 function deletetaskCallback() {
-	
+
 	return null;
 }
 
 function noTask() {
-	
+
 	return null;
 }
 
 function showTask() {
-	
+
 	return null;
 }
 
 function updateTask() {
-	
+
 	return null;
 }
 
@@ -59,8 +63,23 @@ function updateTask() {
 
 // Skjønner ikke helt hvordan han mener at vi skal kunne fjerne på index uten å ta inn listen som parameter også?
 function removeTask(taskID) {
-	
+
 	taskList.splice(taskID, 1);
+}
+
+function content() {
+	const root = document.createElement('div');
+	const content = `
+		
+		<p> Waiting for server data </p> 
+				
+		<button type="button" > New Task </button>
+
+		`;
+
+	root.insertAdjacentHTML('beforeend', content);
+	return root;
+
 }
 
 
---
+++ WebContent/js/module.js
@@ -0,0 +1,4 @@
+import TaskList from './TaskList.js';
+
+//Create tag <task-list> for the new element
+customElements.define('task-list', TaskList);
